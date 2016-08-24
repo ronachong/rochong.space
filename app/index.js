a07726d8901a0b946f71d539f0c6370f5bc083c7
Rona Chong <ronachong@gmail.com>
2016-08-23 19:30:12 -0700

    Incorporate footer component into index.js.

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header.js';
 import Menu from './components/menu.js';
+import Footer from './components/footer.js';
 
 var Site = React.createClass({
     getInitialState: function() {
@@ -17,6 +18,7 @@ var Site = React.createClass({
                 <Header title={this.state.title} />
                 <Menu />
                 <p>Text</p>
+                <Footer />
             </div>
         )
     }
