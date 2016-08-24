97d82d42d90e82a0e55f286bb91039c041d56cdc
Rona Chong <ronachong@gmail.com>
2016-08-23 19:11:59 -0700

    Create basic header component.

---
+++ app/components/header.js
@@ -0,0 +1,11 @@
+console.log("loading header");
+
+import React from 'react';
+
+const Header = (props) => (
+    <header>
+        <h1>{props.title}</h1>
+    </header>
+);
+
+export default Header;
