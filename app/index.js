03fe467fa4de32c5fc96d442840962a7051a7ebf
Rona Chong <ronachong@gmail.com>
2016-08-23 20:47:51 -0700

    Incorporate sitebody component into index.js.
    Import SiteBody and add between menu and footer elements.

--- app/index.js
+++ app/index.js
@@ -2,8 +2,10 @@ console.log("loading index");
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Header from './components/header.js';
 import Menu from './components/menu.js';
+import SiteBody from './components/sitebody.js'
 import Footer from './components/footer.js';
 
 const styles = {
@@ -25,7 +27,7 @@ var Site = React.createClass({
             <div style={styles.main} >
                 <Header title={this.state.title} />
                 <Menu />
-                <p>Text</p>
+                <SiteBody />
                 <Footer />
             </div>
         )
