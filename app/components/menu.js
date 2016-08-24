7876d9f7baf635a36e1b6f09a9ceb1a6e1d28e3d
Rona Chong <ronachong@gmail.com>
2016-08-23 19:20:04 -0700

    Create basic menu component.

---
+++ app/components/menu.js
@@ -0,0 +1,13 @@
+console.log("loading menu");
+
+import React from 'react';
+
+const Menu = (props) => (
+    <ul>
+        <li>Main</li>
+        <li>About</li>
+        <li>Resume</li>
+    </ul>
+);
+
+export default Menu;
