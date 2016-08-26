d473a6d998c8e6f645b16df17db4000f33fa38ce
Rona Chong <ronachong@gmail.com>
2016-08-25 20:12:48 -0700

    Create generic_styles file to house styles to apply to main cmps.
    Create separate file to hold generic (styles) constant, since this
    object will be used by several components.
    
    Create dev styles to help differentiate elements when developing.
    Create site styles to apply to main site components (header, menu,
    siteBody, and footer) and give left padding of 2 em so text is not
    flush to left browser edge.

---
+++ app/generic_styles.js
@@ -0,0 +1,14 @@
+console.log("loading generic styles");
+
+import React from 'react';
+
+const generic = {
+    site : {
+        paddingLeft: '2em'
+    },
+    dev : {
+        borderStyle: 'solid'
+    }
+};
+
+export default generic;
