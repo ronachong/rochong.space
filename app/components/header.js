232ea2e88f7cd4ed6535e63687a5452f6e1e70ed
Rona Chong <ronachong@gmail.com>
2016-08-25 21:14:59 -0700

    Style font of header: font Dosis, align center, and size 4.2em.

--- app/components/header.js
+++ app/components/header.js
@@ -3,9 +3,17 @@ console.log("loading header");
 import React from 'react';
 import generic from '../generic_styles.js'
 
+const styles = {
+    title : {
+        fontFamily: "'Dosis', sans-serif",
+        fontSize: '4.2em',
+        textAlign: 'center'
+    }
+};
+
 const Header = (props) => (
     <header style={Object.assign({}, generic.dev)} >
-        <h1>{props.title}</h1>
+        <h1 style={styles.title} >{props.title}</h1>
     </header>
 );
 
