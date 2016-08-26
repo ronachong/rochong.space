96bb5beed1ff3c58698fe483f53fbec1e31d0c2c
Rona Chong <ronachong@gmail.com>
2016-08-25 22:07:58 -0700

    Add subtle gradient to header.
    Leave backgroudColor specified in case browser does not support
    gradient.

--- app/components/header.js
+++ app/components/header.js
@@ -5,7 +5,9 @@ import generic from '../generic_styles.js'
 
 const styles = {
     header : {
-        backgroundColor: '#26a65b'
+        backgroundColor: '#26a65b',
+        //background: 'linear-gradient(to bottom, rgba(38,166,91,1), rgba(4,147,104,1))'
+        background: 'linear-gradient(to bottom, rgba(38,166,91,1), rgba(30,155,95,1))'
     },
     title : {
         fontFamily: "'Dosis', sans-serif",
