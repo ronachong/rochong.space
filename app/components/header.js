de275a758da4895cd5e1448c9c6a154b2f1f212e
Rona Chong <ronachong@gmail.com>
2016-08-25 21:52:55 -0700

    Adjust header color scheme.

--- app/components/header.js
+++ app/components/header.js
@@ -4,15 +4,19 @@ import React from 'react';
 import generic from '../generic_styles.js'
 
 const styles = {
+    header : {
+        backgroundColor: '#26a65b'
+    },
     title : {
         fontFamily: "'Dosis', sans-serif",
         fontSize: '4.2em',
+        color: 'white',
         textAlign: 'center'
     }
 };
 
 const Header = (props) => (
-    <header style={Object.assign({}, generic.dev)} >
+    <header style={Object.assign({}, styles.header, generic.dev)} >
         <h1 style={styles.title} >{props.title}</h1>
     </header>
 );
