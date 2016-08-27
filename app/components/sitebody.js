80f5094e499b6c8a35257e57b57310e75fd363bd
Rona Chong <ronachong@gmail.com>
2016-08-26 17:07:36 -0700

    Change sitebody cmp from constant to dynamic component.
    Declare SiteBody as var instead of const.
    Implement if conditionals to change return depending on view from
    parent component, site.
    
    Create MainView component as first view option for sitebody.
    MainView (so far) consists of two sub-componenents: a body com-
    ponent to display content and a menu component to change what
    content will be displayed.
    Style the main view so that the body and menu are arranged
    horizontally, and take up as much space as is available.
    Initialize view of the main view to be the blog.

--- app/components/sitebody.js
+++ app/components/sitebody.js
@@ -1,18 +1,43 @@
 console.log("loading site body");
 
 import React from 'react';
+
 import generic from '../generic_styles.js'
+import MainBody from './main_body.js'
+import MainMenu from './main_menu.js'
 
 const styles = {
     sitebody : {
-        flexGrow: 1,
+        flexGrow: 1,            // ensure that sitebody expands to space available
     },
+    mainview : {
+        display: 'flex',        // ensure that cmps in sitebody can expand
+        flexDirection: 'row'    // arrange cmps in sitebody horizontally
+    }
 };
 
-const SiteBody = (props) => (
-    <section style={Object.assign({}, styles.sitebody, generic.dev)} >
-        Body text here.
-    </section>
-);
+var SiteBody = React.createClass({
+    render: function(){
+        if (this.props.view == 'main') {
+            return (<MainView />)
+        }
+    }
+})
+
+var MainView = React.createClass({
+    getInitialState: function() {
+        return {
+            view: 'blog'
+        }
+    },
+    render: function(){
+        return (
+            <section style={Object.assign({}, styles.sitebody, styles.mainview, generic.dev)} >
+                <MainBody view={this.state.view} />
+                <MainMenu />
+            </section>
+        )
+    }
+})
 
 export default SiteBody;
