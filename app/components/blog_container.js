651f3557204482b3c602f7948082df32d30e01e5
Rona Chong <ronachong@gmail.com>
2016-08-26 20:36:33 -0700

    Move blog components into their own file.
    For organization purposes.

---
+++ app/components/blog_container.js
@@ -0,0 +1,49 @@
+console.log("loading blog container");
+
+import React from 'react';
+
+import generic from '../generic_styles.js'
+
+const styles = {
+    mainbody : {
+        width: '80%'
+    },
+    blog : {
+        //
+    }
+};
+
+var tobeimported = [
+    {
+        id: 1,
+        title: "Foo title",
+        text: "Foo text"
+    },
+    {
+        id: 2,
+        title: "Foo title 2",
+        text: "Foo text 2"
+    }
+];
+
+var BlogContainer = React.createClass({
+    render: function(){
+        var blogs = tobeimported.map(function(entry){
+            return <Blog title={entry.title} text={entry.text} key={entry.id} />
+        })
+        return (
+            <section style={Object.assign({}, styles.mainbody, generic.dev)} >
+                {blogs}
+            </section>
+        )
+    }
+});
+
+const Blog = (props) => (
+    <section style={Object.assign({}, styles.blog, generic.dev)} >
+        <h2>{props.title}</h2>
+        <p>{props.text}</p>
+    </section>
+);
+
+export default BlogContainer;
--- app/components/main_body.js
+++ app/components/main_body.js
@@ -1,17 +1,9 @@
-console.log("loading site body");
+console.log("loading main body");
 
 import React from 'react';
 
-import generic from '../generic_styles.js'
+import BlogContainer from './blog_container.js'
 
-const styles = {
-    mainbody : {
-        width: '80%'
-    },
-    blog : {
-        //
-    }
-};
 
 var MainBody = React.createClass({
     render: function(){
@@ -20,39 +12,5 @@ var MainBody = React.createClass({
         }
     }
 });
-
-
-var tobeimported = [
-    {
-        id: 1,
-        title: "Foo title",
-        text: "Foo text"
-    },
-    {
-        id: 2,
-        title: "Foo title 2",
-        text: "Foo text 2"
-    }
-];
-
-var BlogContainer = React.createClass({
-    render: function(){
-        var blogs = tobeimported.map(function(entry){
-            return <Blog title={entry.title} text={entry.text} key={entry.id} />
-        })
-        return (
-            <section style={Object.assign({}, styles.mainbody, generic.dev)} >
-                {blogs}
-            </section>
-        )
-    }
-});
-
-const Blog = (props) => (
-    <section style={Object.assign({}, styles.blog, generic.dev)} >
-        <h2>{props.title}</h2>
-        <p>{props.text}</p>
-    </section>
-);
 
 export default MainBody;
