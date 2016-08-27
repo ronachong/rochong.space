5678f637382df131f83e2ea63b638b0d79aab0ce
Rona Chong <ronachong@gmail.com>
2016-08-26 21:10:25 -0700

    Make menu options in main menu circular, posxned evenly.
    Create h3 elements to house text inside buttons, since text can
    then be positioned center using flex properties.

--- app/components/main_menu.js
+++ app/components/main_menu.js
@@ -8,23 +8,41 @@ const styles = {
         width: '20%',
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
         justifyContent: 'space-between',
         listStyle: 'none',
-        padding: '3% 0 25% 0',      // might have to mess w/ later to posxn li's
+        padding: 0,
         marginBottom: 0
     },
     li : {
-        textAlign: 'center',
-        //flexGrow: 1
+        width: '13vw',              // make li's relative to window, circular
+        height: '13vw',             // make li's relative to window, circular
+        borderRadius: '50%',        // make li's circular
+        display: 'flex',            // enable flex for posxning of children
+        justifyContent: 'center',   // center h3's inside li's, horizontally
+        alignItems: 'center',       // center h3's inside li's, vertically
+        margin: '20% 0'             // vertical breathing rm around li's
+    },
+    h3 : {
+        fontSize: '1em',
+        margin: 0
     }
 };
 
 const MainMenu = (props) => (
     <ul style={Object.assign({}, styles.menu, generic.dev)} >
-        <li style={Object.assign({}, styles.li, generic.dev)} >Blog</li>
-        <li style={Object.assign({}, styles.li, generic.dev)} >Live Projects</li>
-        <li style={Object.assign({}, styles.li, generic.dev)} >Resource Log</li>
-        <li style={Object.assign({}, styles.li, generic.dev)} >Post-Mortems</li>
+        <li style={Object.assign({}, styles.li, generic.dev)} >
+            <h3 style={styles.h3} >Blog</h3>
+        </li>
+        <li style={Object.assign({}, styles.li, generic.dev)} >
+            <h3 style={styles.h3} >Live Projects</h3>
+        </li>
+        <li style={Object.assign({}, styles.li, generic.dev)} >
+            <h3 style={styles.h3} >Resource Log</h3>
+        </li>
+        <li style={Object.assign({}, styles.li, generic.dev)} >
+            <h3 style={styles.h3} >Post-Mortems</h3>
+        </li>
     </ul>
 );
 
