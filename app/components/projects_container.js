9c81ccf2b2ceb3fc95a18431ff79e23ab71317bc
Rona Chong <ronachong@gmail.com>
2016-08-28 13:45:26 -0700

    Adjust proportions and aspect ratios of iframes for live projects.
    Set dimensions of each iframe to 1100 x 1200 px.
    Then use same "msZoom" and webkit fixes as in kusamochi to scale
    preview down to 1/4 of original size.
    Embed iframes in div container and use overflow: hidden to hide
    extra space in 1100 x 1200 canvas now that preview occupies only
    1/4 of original space.
    
    Set height and width of iframe containers to 275 x 300 to match
    iframe, since iframe dimensions are fixed. May change both to be
    responsive later.

--- app/components/projects_container.js
+++ app/components/projects_container.js
@@ -12,6 +12,22 @@ const styles = {
         display: 'flex',            // set flex such that children can be posxned
         flexDirection: 'column',    // arrange children vertically
         alignItems: 'center'        // center content horizontally
+    },
+    iframe_container : {
+        width: '275px',
+        height: '300px',
+        overflow: 'hidden'          // hide overflow from iframe
+    },
+    iframe : {
+        width: '1100px',
+        height: '1200px',
+        msZoom: '0.25',
+        MozTransform: 'scale(.25)',
+        MozTransformOrigin: '0 0',
+        OTransform: 'scale(.25)',
+        OTransformOrigin: '0 0',
+        WebkitTransform: 'scale(.25)',
+        WebkitTransformOrigin: '0 0'
     }
 };
 
@@ -26,31 +42,31 @@ const tobeimported = [
         id: 2,
         title: "Twitter Clone",
         text: "Foo text 2",
-        url: "https://www.kusagashi.com"
+        url: "http://stilettoheelsteam.net"
     },
     {
         id: 3,
         title: "GoldCrests",
         text: "Foo text 2",
-        url: "https://www.kusagashi.com"
+        url: "http://stilettoheelsteam.net"
     },
     {
         id: 4,
         title: "WikiKitty",
         text: "Foo text 2",
-        url: "https://www.kusagashi.com"
+        url: "http://stilettoheelsteam.net"
     },
     {
         id: 5,
         title: "Double Lariat",
         text: "Foo text 2",
-        url: "https://www.kusagashi.com"
+        url: "http://stilettoheelsteam.net"
     },
     {
         id: 6,
         title: "Kusamochi",
         text: "Foo text 2",
-        url: "https://www.kusagashi.com"
+        url: "http://stilettoheelsteam.net"
     }
 ];
 
@@ -77,7 +93,11 @@ const Project = (props) => (
         <h2>
             <a href={props.url} >{props.title}</a>
         </h2>
-        <a href={props.url} ><iframe src={props.url} ></iframe></a>
+        <a href={props.url} >
+            <div style={styles.iframe_container} >
+                <iframe src={props.url} style={styles.iframe} ></iframe>
+            </div>
+        </a>
         <p>{props.text}</p>
     </section>
 );
