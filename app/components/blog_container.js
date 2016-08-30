b735f79fc7ac73c1ec7fff673daff0560f642ef4
Rona Chong <ronachong@gmail.com>
2016-08-29 18:04:19 -0700

    Add dates to blog components.

--- app/components/blog_container.js
+++ app/components/blog_container.js
@@ -9,8 +9,15 @@ const styles = {
         width: '80%',
         overflowY: 'auto'
     },
-    blog : {
-        //
+    blogspan : {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    blogdate : {
+        marginLeft: '7%',
+        paddingTop: '1em',
+        fontSize: '1em',
+        fontStyle: 'italic'
     }
 };
 
@@ -18,21 +25,25 @@ var tobeimported = [
     {
         id: 1,
         title: "Foo title",
+        date: '( 8/29/2016 )',
         text: "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?"
     },
     {
         id: 2,
         title: "Foo title 2",
+        date: '( 8/29/2016 )',
         text: "At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat…"
     },
     {
         id: 3,
         title: "Foo title 3",
+        date: '( 8/29/2016 )',
         text: "At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat…"
     },
     {
         id: 4,
         title: "Foo title 4",
+        date: '( 8/29/2016 )',
         text: "At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat…"
     }
 ];
@@ -40,7 +51,12 @@ var tobeimported = [
 var BlogContainer = React.createClass({
     render: function(){
         var blogs = tobeimported.map(function(entry){
-            return <Blog title={entry.title} text={entry.text} key={entry.id} />
+            return <Blog
+                title={entry.title}
+                date={entry.date}
+                text={entry.text}
+                key={entry.id}
+            />
         });
         return (
             <section style={Object.assign({}, styles.mainbody, generic.dev)} >
@@ -52,7 +68,10 @@ var BlogContainer = React.createClass({
 
 const Blog = (props) => (
     <section style={Object.assign({}, styles.blog, generic.dev)} >
-        <h2 style={generic.textGreen} >{props.title}</h2>
+        <span style={styles.blogspan} >
+            <h2 style={generic.textGreen} >{props.title}</h2>
+            <h3 style={styles.blogdate} >{props.date}</h3>
+        </span>
         <p>{props.text}</p>
     </section>
 );
