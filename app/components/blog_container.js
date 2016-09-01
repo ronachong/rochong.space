55240b06aec72b5dfd73a8ccfa94679e23f1b928
Rona Chong <ronachong@gmail.com>
2016-09-01 16:02:10 -0700

    Create separate file for first blog post; implement into site.
    Refactor blog component into React class so as to parse text into
    individual paragraph elements.

--- app/components/blog_container.js
+++ app/components/blog_container.js
@@ -2,7 +2,8 @@ console.log("loading blog container");
 
 import React from 'react';
 
-import generic from '../generic_styles.js'
+import generic from '../generic_styles.js';
+import text from '../content/2016-09-01_opening-post.js';
 
 const styles = {
     mainbody : {
@@ -21,39 +22,30 @@ const styles = {
         paddingTop: '1em',
         fontSize: '1em',
         fontStyle: 'italic'
+    },
+    p_indented : {
+        margin: '3% 5%',
+        fontFamily: 'Dosis'
     }
 };
+
+var text_20160901_00 = text.split("\n");
 
 var tobeimported = [
     {
         id: 1,
-        title: "Foo title",
-        date: '( 8/29/2016 )',
-        text: "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?"
-    },
-    {
-        id: 2,
-        title: "Foo title 2",
-        date: '( 8/29/2016 )',
-        text: "At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat…"
-    },
-    {
-        id: 3,
-        title: "Foo title 3",
-        date: '( 8/29/2016 )',
-        text: "At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat…"
-    },
-    {
-        id: 4,
-        title: "Foo title 4",
-        date: '( 8/29/2016 )',
-        text: "At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat…"
+        title: "Opening Post",
+        date: '( 9/01/2016 )',
+        text: text_20160901_00
     }
 ];
 
 var BlogContainer = React.createClass({
     render: function(){
         var blogs = tobeimported.map(function(entry){
+            console.log(text_20160901_00);
+            console.log(entry.id);
+            console.log(entry.text);
             return <Blog
                 title={entry.title}
                 date={entry.date}
@@ -69,14 +61,25 @@ var BlogContainer = React.createClass({
     }
 });
 
-const Blog = (props) => (
-    <section style={Object.assign({}, styles.blog, generic.dev)} >
-        <span style={styles.blogspan} >
-            <h2 style={generic.textGreen} >{props.title}</h2>
-            <h3 style={styles.blogdate} >{props.date}</h3>
-        </span>
-        <p>{props.text}</p>
-    </section>
-);
+var Blog = React.createClass({
+    render : function() {
+        var blogParagraphs = this.props.text.map(function(paragraph) {
+            return (
+                (paragraph[0] == '*') ? (
+                    <p style={styles.p_indented} >{paragraph}</p>
+                ) : <p>{paragraph}</p>
+            );
+        });
+        return (
+            <section style={Object.assign({}, styles.blog, generic.dev)} >
+                <span style={styles.blogspan} >
+                    <h2 style={generic.textGreen} >{this.props.title}</h2>
+                    <h3 style={styles.blogdate} >{this.props.date}</h3>
+                </span>
+                {blogParagraphs}
+            </section>
+        )
+    }
+});
 
 export default BlogContainer;
