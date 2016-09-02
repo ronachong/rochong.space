e5d6c19bdf217da42ba25326c40cb3da126bc9ab
Rona Chong <ronachong@gmail.com>
2016-09-01 22:26:52 -0700

    Add content to about view.

--- app/components/about_view.js
+++ app/components/about_view.js
@@ -6,12 +6,23 @@ const styles = {
     mainview : {
         display: 'flex',        // ensure that cmps in sitebody can expand
         flexDirection: 'row'    // arrange cmps in sitebody horizontally
-    }
+    },
+    h2 : generalized.contentHeadings
 };
 
 const AboutView = (props) => (
     <section style={styles.aboutContainer} >
-        <h2>About rochong.space</h2>
+        <h2 style={styles.h2} >About rochong.space</h2>
+        <p>Short for Rona Chong, rochong.space serves as a portfolio/means of archival/means of sharing for the software engineering work and learning that I do.</p>
+
+        <h2 style={styles.h2} >About Rona</h2>
+        <p>I'm curious to know about all the parts that make up the virtual terrain we live in today. Luckily, I attend a school for full-stack software engineering, which means I get to touch a lot of different things: low-level programming, higher-level programming, front-end, back-end, and sysadmin/devops. My goal is to become a well-versed software engineer who knows her stuff.</p>
+        <p>Prior to Holberton School, I got my Bachelor's of Art in sociology from Pomona College and graduated from Scripps, The Women's/Non-normative Gender Identities College (slightly tongue-in-cheek, but still serious). LA/Inland Empire is like a second home to me! After the Central Valley, the area I grew up in, and San Francisco, which serves as my current, albeit overly-foggy, home.</p>
+        <p>Projects that I hope to do one day include a sustainable and community-oriented platform for manga consumption and a mobile app for cognitive-behavioral self-help.</p>
+        <p>Outside of software engineering and tech, I care about social justice and equity, creative storytelling, and pursuing the simple pleasures of life.</p>
+
+        <h2 style={styles.h2} >Contact me</h2>
+        <p>If you are interested in collaborating with me on a project or simply would like to connect with me on a topic related to tech, you can find me on twitter as @ronachng, contact me via LinkedIn, or shoot me an email at rona@holbertonschool.com.</p>
     </section>
 );
 
